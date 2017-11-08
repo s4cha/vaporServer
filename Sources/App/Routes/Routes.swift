8c9a67854f5ad868a0f129dc4e1bd676fb2b1eec
S4cha <sachadso@gmail.com>
2017-11-08 09:43:57 +0100

    Adds Xcode Project

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,15 +1,19 @@
 import Vapor
 
 extension Droplet {
+    
     func setupRoutes() throws {
+    
+        
         get("hello") { req in
             var json = JSON()
             try json.set("hello", "world")
+            try json.set("userID", 12)
             return json
         }
 
         get("plaintext") { req in
-            return "Hello, world!"
+            return "Hello, you!"
         }
 
         // response to requests to /info domain
@@ -19,6 +23,20 @@ extension Droplet {
         }
 
         get("description") { req in return req.description }
+        
+        
+        
+         get("banana") { req in
+            self.log.warning("o")
+            self.log.error("Dayum")
+            self.log.fatal("FATALLLLL")
+            
+            self.log.info("Informational log")
+            
+            let digest = try self.hash.make("vapor")
+            return digest.makeString()
+            
+        }
         
         try resource("posts", PostController.self)
     }
